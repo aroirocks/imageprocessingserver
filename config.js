3bfae6a7c39f3bd9340c78dba3cc9051e7f71f5d
Amit Rai <60046142+aroirocks@users.noreply.github.com>
2020-09-03 15:28:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = keys = {
-  db: "Mongodb URI",
+  db: "mongodb://amit:amit@devconnector-shard-00-00.ixhwi.mongodb.net:27017,devconnector-shard-00-01.ixhwi.mongodb.net:27017,devconnector-shard-00-02.ixhwi.mongodb.net:27017/sessions?ssl=true&replicaSet=devconnector-shard-0&authSource=admin&retryWrites=true&w=majority",
 };
